addafdb2ccc5c16561c82e5e0f9b6c2b7a3d4255
John Wright <johnlarrywright.ITspecialist@gmail.com>
2019-08-04 02:40:37 +0000

    finally settimeout to 1

--- port.py
+++ port.py
@@ -8,8 +8,30 @@ parser = argparse.ArgumentParser(
     description='Command line Scanning ports'
 )
 
-parser.add_argument('port_scanner', help='Require ip in order to scan for open ports')
+parser.add_argument('ip', help='Require ip in order to scan for open ports')
 
 args = parser.parse_args()
 
-port_scanner = args.port_scanner
+ip_scan = args.ip
+
+#open_ports = socket.gethostbyname(port_scanner)
+#family = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#result = family.connect(('68.66.200.198', 443))
+
+for port_scan in range(1, 10):
+    family = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    family.settimeout(1)
+    print('socket created')
+    try:
+        result = family.connect(('13.33.147.123', 443))
+    
+        print('Port 80: OPEN', family)
+    except:
+        print('timeout')
+    
+
+#print('Enter ip address {}'.format(port_scanner))
+#print('Enter IP address {}'.format(open_ports))
+#print('Enter ip address {}'.format(port_scanner))
+
+
