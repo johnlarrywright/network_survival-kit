dbcf6286d902c8e547cf58c527e4bcaf5146ec53
John Wright <johnlarrywright.ITspecialist@gmail.com>
2019-08-06 07:43:04 +0000

    almost finish

--- client_browser.py
+++ client_browser.py
@@ -3,20 +3,24 @@
 import urllib.request
 import argparse
 
-parser = argparse.ArgumentParser(
-    prog ='Client Browser',
-    description='Command line Client Browser'
+def client():
+    parser = argparse.ArgumentParser(
+        prog ='Client Browser',
+        description='Command line Client Browser'
 )
 
-parser.add_argument('client', help='Enter domain for browser')
+    parser.add_argument('client', help='Enter domain for browser')
 
-args = parser.parse_args()
+    args = parser.parse_args()
 
-client = args.client
+    client = args.client
 
-url = urllib.request.urlopen(client)
+    url = urllib.request.urlopen(client)
 
 #print(url.read())
-print(url.geturl())
-print(url.info())
-print(url.getcode())
+    print(url.geturl())
+    print(url.info())
+    print(url.getcode())
+    
+if __name__ == "__main__":
+    client()    
--- data_collector.py
+++ data_collector.py
@@ -3,10 +3,12 @@
 import system_tool 
 import network
 import port
-#import client_browser
-#import mac_lookup
+import client_browser
+import mac_lookup
 
 if __name__ == "__main__":
     system_tool.system()
     network.network()
     port.port()
+    client_browser.client()
+    mac_lookup.mac()
